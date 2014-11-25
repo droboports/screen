4dd77c47823907b3fbe7a6da35fef8cd0c303246
Ricardo Padilha <ricardospadilha@gmail.com>
2014-11-25 12:37:16 +0100

    First version

---
+++ build.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+### bash best practices ###
+# exit on error code
+set -o errexit
+# exit on unset variable
+set -o nounset
+# return error of last failed command in pipe
+set -o pipefail
+# expand aliases
+shopt -s expand_aliases
+# print trace
+set -o xtrace
+
+### logfile ###
+timestamp="$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="logfile_${timestamp}.txt"
+echo "${0} ${@}" > "${logfile}"
+# save stdout to logfile
+exec 1> >(tee -a "${logfile}")
+# redirect errors to stdout
+exec 2> >(tee -a "${logfile}" >&2)
+
+### environment variables ###
+source crosscompile.sh
+export NAME="screen"
+export DEST="/mnt/DroboFS/Shares/DroboApps/${NAME}"
+export DEPS="/mnt/DroboFS/Shares/DroboApps/${NAME}deps"
+export CFLAGS="$CFLAGS -Os -fPIC"
+export CXXFLAGS="$CXXFLAGS $CFLAGS"
+export CPPFLAGS="-I${DEPS}/include"
+export LDFLAGS="${LDFLAGS:-} -Wl,-rpath,${DEST}/lib -L${DEST}/lib"
+alias make="make -j8 V=1 VERBOSE=1"
+
+# $1: file
+# $2: url
+# $3: folder
+_download_tgz() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  [[ ! -d "target/${3}" ]] && tar -zxvf "download/${1}" -C target
+  return 0
+}
+
+# $1: file
+# $2: url
+# $3: folder
+_download_app() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  mkdir -p "target/${3}"
+  tar -zxvf "download/${1}" -C target/${3}
+  return 0
+}
+
+# $1: branch
+# $2: folder
+# $3: url
+_download_git() {
+  [[ -d "target/${2}" ]] && rm -v -fr "target/${2}"
+  [[ ! -d "target/${2}" ]] && git clone --branch "${1}" --single-branch --depth 1 "${3}" "target/${2}"
+  return 0
+}
+
+# $1: file
+# $2: url
+_download_file() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  return 0
+}
+
+### NCURSES ###
+_build_ncurses() {
+local VERSION="5.9"
+local FOLDER="ncurses-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://ftp.gnu.org/gnu/ncurses/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --host=arm-none-linux-gnueabi --prefix="${DEPS}" --libdir="${DEST}/lib" --datadir="${DEST}/share" --with-shared --enable-rpath
+make
+make install
+rm -v "${DEST}/lib"/*.a
+popd
+}
+
+### SCREEN ###
+_build_screen() {
+local VERSION="4.2.1"
+local FOLDER="screen-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://ftp.gnu.org/gnu/screen/${FILE}"
+local SRC="${PWD}/src"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+# see http://savannah.gnu.org/bugs/?43222
+# and http://savannah.gnu.org/bugs/?43223
+# for the patch explanations.
+for p in ${SRC}/*.patch; do
+  patch -p2 < ${p}
+done
+./autogen.sh
+./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --with-pty-mode=0620 --with-pty-group=0
+make
+make install
+mkdir -p "${DEST}/etc"
+cp -v etc/etcscreenrc "${DEST}/etc/screenrc"
+cp -v etc/screenrc "${DEST}/etc/.screenrc"
+popd
+}
+
+### BUILD ###
+_build() {
+#  _build_zlib
+  _build_ncurses
+  _build_screen
+  _package
+}
+
+_create_tgz() {
+  local appname="$(basename ${PWD})"
+  local appfile="${PWD}/${appname}.tgz"
+
+  if [[ -f "${appfile}" ]]; then
+    rm -v "${appfile}"
+  fi
+
+  pushd "${DEST}"
+  tar --verbose --create --numeric-owner --owner=0 --group=0 --gzip --file "${appfile}" *
+  popd
+}
+
+_package() {
+  mkdir -p "${DEST}"
+  cp -avfR src/dest/* "${DEST}"/
+  find "${DEST}" -name "._*" -print -delete
+  _create_tgz
+}
+
+_clean() {
+  rm -v -fr "${DEPS}"
+  rm -v -fr "${DEST}"
+  rm -v -fr target/*
+}
+
+_dist_clean() {
+  _clean
+  rm -v -f logfile*
+  rm -v -fr download/*
+}
+
+case "${1:-}" in
+  clean)     _clean ;;
+  distclean) _dist_clean ;;
+  package)   _package ;;
+  "")        _build ;;
+  *)         _build_${1} ;;
+esac
